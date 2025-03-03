eb9a33f0282d486a80a30b8a8bd6199b257bd946
celuladevarquitectura <celuladev.arquitectura@col.bellota.com>
2025-03-03 17:46:45 -0500

    se realiza creacion de servicios de la entidad pagos

--- src/main/java/com/gestion/plus/api/controller/PagoController.java
+++ src/main/java/com/gestion/plus/api/controller/PagoController.java
@@ -1,8 +1,13 @@
 package com.gestion.plus.api.controller;
 
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gestion.plus.api.services.impl.PagosServiceImpl;
 import com.gestion.plus.commons.dtos.PagosDTO;
+import com.gestion.plus.commons.dtos.PersonaDTO;
 import com.gestion.plus.commons.dtos.ResponseDTO;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -20,24 +26,88 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping({"/api/v1/Pago"})
-@CrossOrigin(origins = {"*"}, methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
+@RequestMapping({ "/api/v1/Pago" })
+@CrossOrigin(origins = { "*" }, methods = { RequestMethod.DELETE, RequestMethod.GET, RequestMethod.POST,
+		RequestMethod.PUT })
 @RequiredArgsConstructor
 public class PagoController {
-	
-	//private final PagosServiceImpl pagosServiceImpl;
-	
-	/*@Operation(summary = "Operacion que permite crear un pago")
-	  @ApiResponses(value = {
-				@ApiResponse(responseCode = "200", description = "Se consulta exitosamente", content = {
-						@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
-				@ApiResponse(responseCode = "400", description = "La petición no puede ser entendida por el servidor debido a errores de sintaxis, el cliente no debe repetirla no sin antes hacer modificaciones", content = {
-						@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
-				@ApiResponse(responseCode = "404", description = "El recurso solicitado no puede ser encontrado", content = {
-						@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
-				@ApiResponse(responseCode = "500", description = "Se presento una condición inesperada que impidió completar la petición", content = {
-						@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }), })@PostMapping
-	  public ResponseEntity<ResponseDTO> savePagos(@RequestBody PagosDTO pagosDTO) {
-	    return this.pagosServiceImpl.savePagos(pagosDTO);
-	  }*/
+
+	private final PagosServiceImpl pagosServiceImpl;
+
+	@Operation(summary = "Operacion que permite crear un pago")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Se consulta exitosamente", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "La petición no puede ser entendida por el servidor debido a errores de sintaxis, el cliente no debe repetirla no sin antes hacer modificaciones", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "404", description = "El recurso solicitado no puede ser encontrado", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "500", description = "Se presento una condición inesperada que impidió completar la petición", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }), })
+	@PostMapping
+	public ResponseEntity<ResponseDTO> savePagos(@RequestBody PagosDTO pagosDTO) {
+		return this.pagosServiceImpl.savePagos(pagosDTO);
+	}
+
+	@Operation(summary = "Operacion que permite consultar pago por id persona")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Se consulta exitosamente", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "La petición no puede ser entendida por el servidor debido a errores de sintaxis, el cliente no debe repetirla no sin antes hacer modificaciones", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "404", description = "El recurso solicitado no puede ser encontrado", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "500", description = "Se presento una condición inesperada que impidió completar la petición", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }), })
+	@GetMapping("/{id}")
+	public ResponseEntity<ResponseDTO> getPagoByIdPersona(@PathVariable Integer id) {
+		PersonaDTO personaDTO = new PersonaDTO();
+		personaDTO.setId(id);
+		return pagosServiceImpl.getPagoByIdPersona(personaDTO);
+	}
+
+	@Operation(summary = "Operacion que permite consultar los pagos")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Se consulta exitosamente", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "La petición no puede ser entendida por el servidor debido a errores de sintaxis, el cliente no debe repetirla no sin antes hacer modificaciones", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "404", description = "El recurso solicitado no puede ser encontrado", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "500", description = "Se presento una condición inesperada que impidió completar la petición", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }), })
+	@GetMapping({ "/all" })
+	public ResponseEntity<ResponseDTO> getAllPagos() {
+		return this.pagosServiceImpl.findAll();
+	}
+
+	@Operation(summary = "Operacion que permite eliminar un pago por id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Se consulta exitosamente", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "La petición no puede ser entendida por el servidor debido a errores de sintaxis, el cliente no debe repetirla no sin antes hacer modificaciones", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "404", description = "El recurso solicitado no puede ser encontrado", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "500", description = "Se presento una condición inesperada que impidió completar la petición", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }), })
+	@DeleteMapping({ "/{id}" })
+	public ResponseEntity<ResponseDTO> deletePago(@PathVariable Integer id) {
+		return this.pagosServiceImpl.deletePago(id);
+	}
+
+	@Operation(summary = "Operacion que permite actualizar pago por id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Se consulta exitosamente", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "400", description = "La petición no puede ser entendida por el servidor debido a errores de sintaxis, el cliente no debe repetirla no sin antes hacer modificaciones", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "404", description = "El recurso solicitado no puede ser encontrado", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }),
+			@ApiResponse(responseCode = "500", description = "Se presento una condición inesperada que impidió completar la petición", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = ResponseDTO.class)) }), })
+	  @PutMapping("/{id}")
+    public ResponseEntity<ResponseDTO> updatePago(@PathVariable Integer id, @RequestBody PagosDTO pagosDTO) {
+        return this.pagosServiceImpl.updatePago(id, pagosDTO);
+    }
 }
