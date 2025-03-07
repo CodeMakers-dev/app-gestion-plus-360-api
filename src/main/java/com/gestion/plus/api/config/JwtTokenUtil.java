4de525bf489384e24db3d9f4facf2e01c40bc970
celuladevarquitectura <celuladev.arquitectura@col.bellota.com>
2025-03-07 15:29:45 -0500

    token

--- src/main/java/com/gestion/plus/api/config/JwtTokenUtil.java
+++ src/main/java/com/gestion/plus/api/config/JwtTokenUtil.java
@@ -82,6 +82,7 @@ public class JwtTokenUtil implements Serializable {
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
     
+    
     public void invalidateToken(String token) {
         invalidatedTokens.add(token);
     }
